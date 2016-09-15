d369c18ad6dee9c4e654378ab5281e6573fd3775
Lithry <facundosargiotti@gmail.com>
2016-09-15 13:10:44 -0300

    Ver 3.0.1 - Archivos agregados

---
+++ EngineDll/Importer.h
@@ -0,0 +1,16 @@
+#ifndef IMPORTER_H
+#define IMPORTER_H
+
+#include <string>
+
+class Mesh;
+
+#define DllExport __declspec( dllexport )
+
+class DllExport Importer{
+public:
+	Importer();
+	~Importer();
+	bool importMesh(const std::string& rkFilename, Mesh& orkMesh);
+};
+#endif
