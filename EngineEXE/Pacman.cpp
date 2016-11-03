c07b28bd866b69c5ccf5274cbc0968fdd8587108
Lithry <facundosargiotti@gmail..com>
2016-11-03 02:34:41 -0300

    Ver 5.1.1 - Importer

--- EngineEXE/Pacman.cpp
+++ EngineEXE/Pacman.cpp
@@ -2,84 +2,36 @@
 #include "input.h"
 #include "Camera.h"
 
-void numpadController(Entity3D* object, Input& rkInput){
-	static float speed = 0.1;
+void OPController(Entity3D* object, Input& rkInput){
+	static float speed = 1;
 
-	if (rkInput.keyDown(Input::KEY_NUMPAD8)){
-		object->setPosZ(object->posZ() + speed);
+	if (rkInput.keyDown(Input::KEY_O)){
+		object->setRotationY(object->rotationY() + speed);
 	}
-	else if (rkInput.keyDown(Input::KEY_NUMPAD5)){
-		object->setPosZ(object->posZ() - speed);
-	}
-	if (rkInput.keyDown(Input::KEY_NUMPAD6)){
-		object->setPosX(object->posX() + speed);
-	}
-	else if (rkInput.keyDown(Input::KEY_NUMPAD4)){
-		object->setPosX(object->posX() - speed);
-	}
-	if (rkInput.keyDown(Input::KEY_ADD)){
-		object->setPosY(object->posY() + speed);
-	}
-	else if (rkInput.keyDown(Input::KEY_SUBTRACT)){
-		object->setPosY(object->posY() - speed);
-	}
-	if (rkInput.keyDown(Input::KEY_NUMPAD9)){
-		object->setRotationY(object->rotationY() + speed * 10);
-	}
-	else if (rkInput.keyDown(Input::KEY_NUMPAD7)){
-		object->setRotationY(object->rotationY() - speed * 10);
-	}
-	if (rkInput.keyDown(Input::KEY_NUMPAD1)){
-		object->setRotationZ(object->rotationZ() + speed * 10);
-	}
-	else if (rkInput.keyDown(Input::KEY_NUMPAD3)){
-		object->setRotationZ(object->rotationZ() - speed * 10);
+	else if (rkInput.keyDown(Input::KEY_P)){
+		object->setRotationY(object->rotationY() - speed);
 	}
 }
 
-void keyController(Entity3D* object, Input& rkInput){
-	static float speed = 0.1;
+void KLController(Entity3D* object, Input& rkInput){
+	static float speed = 1;
 
-	if (rkInput.keyDown(Input::KEY_U)){
-		object->setPosZ(object->posZ() + speed);
-	}
-	else if (rkInput.keyDown(Input::KEY_J)){
-		object->setPosZ(object->posZ() - speed);
-	}
 	if (rkInput.keyDown(Input::KEY_K)){
-		object->setPosX(object->posX() + speed);
-	}
-	else if (rkInput.keyDown(Input::KEY_H)){
-		object->setPosX(object->posX() - speed);
-	}
-	if (rkInput.keyDown(Input::KEY_O)){
-		object->setPosY(object->posY() + speed);
+		object->setRotationZ(object->rotationZ() + speed);
 	}
 	else if (rkInput.keyDown(Input::KEY_L)){
-		object->setPosY(object->posY() - speed);
-	}
-	if (rkInput.keyDown(Input::KEY_I)){
-		object->setRotationY(object->rotationY() + speed * 10);
-	}
-	else if (rkInput.keyDown(Input::KEY_Y)){
-		object->setRotationY(object->rotationY() - speed * 10);
-	}
-	if (rkInput.keyDown(Input::KEY_N)){
-		object->setRotationZ(object->rotationZ() + speed * 10);
-	}
-	else if (rkInput.keyDown(Input::KEY_M)){
-		object->setRotationZ(object->rotationZ() - speed * 10);
+		object->setRotationZ(object->rotationZ() - speed);
 	}
 }
 
-void upDownController9and0(Entity3D* object, Input& rkInput){
-	static float speed = 0.1;
+void NMController(Entity3D* object, Input& rkInput){
+	static float speed = 1;
 
-	if (rkInput.keyDown(Input::KEY_0)){
-		object->setPosY(object->posY() + speed);
+	if (rkInput.keyDown(Input::KEY_N)){
+		object->setRotationX(object->rotationX() + speed);
 	}
-	else if (rkInput.keyDown(Input::KEY_9)){
-		object->setPosY(object->posY() - speed);
+	else if (rkInput.keyDown(Input::KEY_M)){
+		object->setRotationX(object->rotationX() - speed);
 	}
 }
 
@@ -89,7 +41,7 @@ bool Pacman::init(Renderer& rkRenderer){
 
 	node1 = new Node();
 	Importer* importer = new Importer(rkRenderer);
-	importer->importScene("Assets/Scene/testAxisY.dae", *node1);
+	importer->importScene("Assets/Scene/Scene.dae", *node1);
 
 	child1 = node1->childs()[0];
 	child2 = node1->childs()[1];
@@ -100,13 +52,9 @@ bool Pacman::init(Renderer& rkRenderer){
 void Pacman::frame(Renderer& rkRenderer, Input& rkInput, Timer& rkTimer){
 	cam->controls(rkInput);
 	
-
-	
-
-	numpadController(child2, rkInput);
-
-	
-	
+	OPController(node1, rkInput);
+	KLController(child2, rkInput);
+	NMController(child1, rkInput);
 
 	node1->updateTransformation();
 	node1->draw();
